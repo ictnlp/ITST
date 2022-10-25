160fe8aebaa1a13331d032ab9983e38121f231e8
Vily1998 <2512857469@qq.com>
2022-10-25 01:57:30 +0000

    first commit

--- scripts/average_checkpoints.py
+++ scripts/average_checkpoints.py
@@ -6,11 +6,10 @@
 
 import argparse
 import collections
+import torch
 import os
 import re
-
-import torch
-from fairseq.file_io import PathManager
+import pdb
 
 
 def average_checkpoints(inputs):
@@ -29,14 +28,13 @@ def average_checkpoints(inputs):
     new_state = None
     num_models = len(inputs)
 
-    for fpath in inputs:
-        with PathManager.open(fpath, "rb") as f:
-            state = torch.load(
-                f,
-                map_location=(
-                    lambda s, _: torch.serialization.default_restore_location(s, "cpu")
-                ),
-            )
+    for f in inputs:
+        state = torch.load(
+            f,
+            map_location=(
+                lambda s, _: torch.serialization.default_restore_location(s, "cpu")
+            ),
+        )
         # Copies over the settings from the first checkpoint
         if new_state is None:
             new_state = state
@@ -65,35 +63,68 @@ def average_checkpoints(inputs):
     averaged_params = collections.OrderedDict()
     for k, v in params_dict.items():
         averaged_params[k] = v
-        if averaged_params[k].is_floating_point():
-            averaged_params[k].div_(num_models)
-        else:
-            averaged_params[k] //= num_models
+        averaged_params[k].div_(num_models)
     new_state["model"] = averaged_params
     return new_state
 
 
-def last_n_checkpoints(paths, n, update_based, upper_bound=None):
+def last_n_checkpoints(
+    paths, n, update_based, best_base, upper_bound=None, last=None, interval=None
+):
     assert len(paths) == 1
     path = paths[0]
     if update_based:
         pt_regexp = re.compile(r"checkpoint_\d+_(\d+)\.pt")
     else:
         pt_regexp = re.compile(r"checkpoint(\d+)\.pt")
-    files = PathManager.ls(path)
+    if best_base:
+        pt_regexp = re.compile(r"checkpoint.best_accuracy_(\d\d.\d\d).pt")
+    files = os.listdir(path)
 
     entries = []
+    last_key = None
     for f in files:
         m = pt_regexp.fullmatch(f)
         if m is not None:
-            sort_key = int(m.group(1))
+            # pdb.set_trace()
+            if not best_base:
+                sort_key = int(m.group(1))
+            else:
+                sort_key = float(m.group(1))
             if upper_bound is None or sort_key <= upper_bound:
+                # pdb.set_trace()
                 entries.append((sort_key, m.group(0)))
+                if last is not None:
+                    # pdb.set_trace()
+                    if m.group(0).split("_")[-1] == last.split("_")[-1]:
+                        last_key = sort_key
     if len(entries) < n:
         raise Exception(
             "Found {} checkpoint files but need at least {}", len(entries), n
         )
-    return [os.path.join(path, x[1]) for x in sorted(entries, reverse=True)[:n]]
+    # pdb.set_trace()
+    # print(entries)
+    newnew_entries = []
+    if last_key is not None:
+        new_entries = sorted(entries)
+        # pdb.set_trace()
+        for entry in new_entries:
+            if interval is None:
+                if entry[0] <= last_key:
+                    newnew_entries.append(entry)
+            else:
+                if entry[0] <= last_key and (last_key - entry[0]) % interval == 0:
+                    newnew_entries.append(entry)
+
+        if len(newnew_entries) > 5:
+            return [
+                os.path.join(path, x[1])
+                for x in sorted(newnew_entries, reverse=True)[:n]
+            ]
+        else:
+            return [os.path.join(path, x[1]) for x in sorted(entries, reverse=True)[:n]]
+    else:
+        return [os.path.join(path, x[1]) for x in sorted(entries, reverse=True)[:n]]
 
 
 def main():
@@ -104,6 +135,8 @@ def main():
     # fmt: off
     parser.add_argument('--inputs', required=True, nargs='+',
                         help='Input checkpoint file paths.')
+    parser.add_argument('--last_file',
+                        help='last checkpoint, add by Shaolei Zhang') # add by Shaolei Zhang
     parser.add_argument('--output', required=True, metavar='FILE',
                         help='Write the new checkpoint containing the averaged weights to this path.')
     num_group = parser.add_mutually_exclusive_group()
@@ -114,11 +147,11 @@ def main():
                            help='if set, will try to find checkpoints with names checkpoint_ee_xx.pt in the path specified by input, '
                            'and average last this many of them.')
     parser.add_argument('--checkpoint-upper-bound', type=int,
-                        help='when using --num-epoch-checkpoints, this will set an upper bound on which epoch to use, '
-                        'when using --num-update-checkpoints, this will set an upper bound on which update to use'
-                        'e.g., with --num-epoch-checkpoints=10 --checkpoint-upper-bound=50, checkpoints 41-50 would be averaged.'
-                        'e.g., with --num-update-checkpoints=10 --checkpoint-upper-bound=50000, checkpoints 40500-50000 would be averaged assuming --save-interval-updates 500'
-                        )
+                        help='when using --num-epoch-checkpoints, this will set an upper bound on which checkpoint to use, '
+                        'e.g., with --num-epoch-checkpoints=10 --checkpoint-upper-bound=50, checkpoints 41-50 would be averaged.')
+    parser.add_argument('--average-interval', type=int,
+                        help='average-interval')
+    parser.add_argument('--best', type=bool,help='best checkpoint, add by Shaolei Zhang') # add by Shaolei Zhang
     # fmt: on
     args = parser.parse_args()
     print(args)
@@ -131,27 +164,38 @@ def main():
     elif args.num_epoch_checkpoints is not None:
         num = args.num_epoch_checkpoints
 
-    assert args.checkpoint_upper_bound is None or (
-        args.num_epoch_checkpoints is not None
-        or args.num_update_checkpoints is not None
-    ), "--checkpoint-upper-bound requires --num-epoch-checkpoints or --num-update-checkpoints"
+    assert (
+        args.checkpoint_upper_bound is None or args.num_epoch_checkpoints is not None
+    ), "--checkpoint-upper-bound requires --num-epoch-checkpoints"
     assert (
         args.num_epoch_checkpoints is None or args.num_update_checkpoints is None
     ), "Cannot combine --num-epoch-checkpoints and --num-update-checkpoints"
 
+    if args.last_file is not None:
+        last = args.last_file
+        last = last.split("/")[-1]
+    else:
+        last = None
+    if args.average_interval is not None:
+        interval = int(args.average_interval)
+    else:
+        interval = None
+    # pdb.set_trace()
+    is_best_based = getattr(args, "best", False)
     if num is not None:
         args.inputs = last_n_checkpoints(
             args.inputs,
             num,
             is_update_based,
+            is_best_based,
             upper_bound=args.checkpoint_upper_bound,
+            last=last,
+            interval=interval,
         )
         print("averaging checkpoints: ", args.inputs)
-
     new_state = average_checkpoints(args.inputs)
-    with PathManager.open(args.output, "wb") as f:
-        torch.save(new_state, f)
-    print("Finished writing averaged checkpoint to {}".format(args.output))
+    torch.save(new_state, args.output)
+    print("Finished writing averaged checkpoint to {}.".format(args.output))
 
 
 if __name__ == "__main__":
